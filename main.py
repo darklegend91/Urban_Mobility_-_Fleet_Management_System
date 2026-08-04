0c29de0deaa6491503a68bfe7e6ed84f86babfdf
Aditya Pathania <adityapathania2194@gmail.com>
2026-08-04 09:32:57 +0530

    update : [Aditya] Made the vehicle class

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World!!")
+"""Use Case 1 
+Define a class Vehicle with __init__ constructor.
+Initialize attributes: vehicle_id, model, and battery_percentage.
+"""
+
+class Vehicle:
+    
+    def __init__(self , vehicle_id: str , model: str , battery_precentage: float) -> None:
+        self.vehicle_id = vehicle_id
+        self.model = model
+        self.battery_precentage = battery_precentage
